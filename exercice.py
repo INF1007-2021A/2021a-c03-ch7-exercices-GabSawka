e3159331823744a9cf98d3afc9866906ba74c3a3
GabSawka <gabriel.sawka@polymtl.ca>
2021-10-25 14:20:36 -0400

    doneithink

--- exercice.py
+++ exercice.py
@@ -4,6 +4,7 @@
 # TODO: Importez vos modules ici
 from math import pi
 import turtle
+from typing import Sequence
 #import sys
 #sys.path.append("../c03-ch6-1-exercices-GabSawka")
 #from c03-ch6-1-exercices-GabSawka.exercice import frequence
@@ -21,7 +22,64 @@ def get_most_use_letter(phrase :str):
     print(dict_arg)
     return list(dict_arg.keys())[0]
 
+def draw(thick,x=0,y=0,orientation=90):
+    turtle.pendown()
+    turtle.width(thick)
+    turtle.color(0,1,0)
+    turtle.setheading(orientation)
+    turtle.forward(20*thick)
+    posi=turtle.pos()
+    for i in range(0,2):
+        turtle.penup()
+        turtle.setpos(posi[0],posi[1])
+        head=orientation+30*(-1)**i
+        if thick !=0:
+            draw(thick-1,posi[0],posi[1],head)
+
+def saisie():
+    c_not_valid=True
+    while(c_not_valid):
+        chaine=str(input("chaine adn: "))
+        c_not_valid=(not valide(chaine))
+    return chaine
+        
+def valide(chaine):
+    car_valide=['a','c','t','g']
+    if(len(chaine)==0):
+        return False
+    for letter in chaine:
+        if letter not in car_valide:
+            return False
+    return True  
+
+def proportion(chaine, seq):
+    nb_tot=len(chaine)-len(seq)+1
+    nb_pres=0
+    
+    for l in range(len(chaine)-len(seq)+1):
+        l_precedant = seq[0]==chaine[l]
+        for s in range(0,len(seq)):
+            if seq[s]!=chaine[l+s] and l_precedant:
+                l_precedant=False 
+        if l_precedant:
+            nb_pres+=1
+    print(nb_tot)
+    print(nb_pres)
+    return nb_pres/nb_tot
+
+
+
+
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
     print(massevolumiqueelipsoide(1,2,3,5))
     print(get_most_use_letter("aaaaaaaaaaa bbbbbbbbbbbbbbbbbbbbbbbbb eeee"))
+    #draw(4)
+    chaine = saisie()
+    sequence = saisie()
+    ratio=proportion(chaine,sequence)
+    print(chaine)
+    print(sequence)
+    print(ratio)
+
+
