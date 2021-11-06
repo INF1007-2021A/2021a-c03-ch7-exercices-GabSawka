435ff586f9f4a1c4a2195faf760d4c08f60c214d
GabSawka <gabriel.sawka@polymtl.ca>
2021-11-05 20:55:10 -0400

    5nov

--- exercice.py
+++ exercice.py
@@ -13,79 +13,75 @@ from typing import Sequence
 from exercice1 import frequence
 
 # TODO: Définissez vos fonction ici
-def massevolumiqueelipsoide(a,b,c,M):
-    vol=4/3*pi*a*b*c
-    return(vol, M*vol)
 
-def get_most_use_letter(phrase :str):
-    dict_arg = frequence(phrase)
-    print(dict_arg)
-    return list(dict_arg.keys())[0]
 
-def draw(thick,orientation=90):
+def massevolumiqueelipsoide(a=0.0, b=0.0, c=0.0, M=0.0):
+    vol = 4/3 * pi*a*b*c
+    return(vol, M * vol)
+
+
+def get_most_use_letter(phrase: str):
+    return list(frequence(phrase).keys())[0]
+
+
+def draw(thick, orientation=90):
+
     turtle.pendown()
     turtle.width(thick*5)
-    turtle.color(0,1,0)
+    turtle.color(0, 1, 0)
     turtle.setheading(orientation)
     turtle.forward(20*thick)
-    posi=turtle.pos()
+    posi = turtle.pos()
     for i in range(2):
         turtle.penup()
-        turtle.setpos(posi[0],posi[1])
-        head=orientation+30*(-1)**i
-        if thick !=0:
-            draw(thick-1,head)
+        turtle.setpos(posi[0], posi[1])
+        if thick != 0:
+            draw(thick-1, orientation+30*(-1)**i)
+
 
 def saisie():
-    c_not_valid=True
+    c_not_valid = True
     while(c_not_valid):
-        chaine=str(input("chaine adn: "))
-        c_not_valid=(not valide(chaine))
+        chaine = str(input("chaine adn: "))
+        c_not_valid = (not valide(chaine))
     return chaine
-        
+
+
 def valide(chaine):
-    car_valide=['a','c','t','g']
-    if(len(chaine)==0):
+    car_valide = ['a', 'c', 't', 'g']
+    if(len(chaine) == 0):
         return False
     for letter in chaine:
         if letter not in car_valide:
             return False
-    return True  
+    return True
+
 
 def proportion(chaine, seq):
-    nb_tot=len(chaine)-len(seq)+1
-    nb_pres=0
-    
+    nb_tot = len(chaine)-len(seq)+1
+    nb_pres = 0
     for l in range(len(chaine)-len(seq)+1):
-        l_precedant = seq[0]==chaine[l]
-        for s in range(0,len(seq)):
-            if seq[s]!=chaine[l+s] and l_precedant:
-                l_precedant=False 
+        l_precedant = seq[0] == chaine[l]
+        for s in range(0, len(seq)):
+            if seq[s] != chaine[l+s] and l_precedant:
+                l_precedant = False
         if l_precedant:
-            nb_pres+=1
-    print(nb_tot)
-    print(nb_pres)
-    return nb_pres/nb_tot
+            nb_pres += 1
+    return nb_pres/nb_tot*100
+
 
 def facto(n):
-    if n==0 or n==1:
+    if n == 0 or n == 1:
         return 1
     else:
         return n*facto(n-1)
 
 
-
 if __name__ == '__main__':
-    # TODO: Appelez vos fonctions ici
-    #print(massevolumiqueelipsoide(1,2,3,5))
-    #print(get_most_use_letter("aaaaaaaaaaa bbbbbbbbbbbbbbbbbbbbbbbbb eeee"))
-    #draw(4)
-    #chaine = saisie()
-    #sequence = saisie()
-    #ratio=proportion(chaine,sequence)
-    #print(chaine)
-    #print(sequence)
-    #print(ratio)
+    print(massevolumiqueelipsoide(1, 2, 3, 5))
+    print(get_most_use_letter("aaaaaaaaaaa bbbbbbbbbbbbbbbbbbbbbbbbb eeee"))
+    draw(4)
+    chaine = saisie()
+    sequence = saisie()
+    ratio = proportion(chaine, sequence)
     print(facto(4))
-
-
